951c5ed5e71cad8cd858d9a8ded3c0034f44e091
The Logic Nomad <chirag748317@gmail.com>
2026-01-10 01:11:53 +0530

    Remove trailing slash from BACKEND_URL

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 //export const BACKEND_URL = "http://localhost:3000"
 
-export const BACKEND_URL = "https://sitecrafter-final-version-backend.vercel.app/"
+export const BACKEND_URL = "https://sitecrafter-final-version-backend.vercel.app"
